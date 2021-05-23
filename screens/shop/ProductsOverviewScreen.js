175450fb77a1234fdab11d6339b5ce1d9004398a
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-05-23 21:20:22 +0300

    styling the product items

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { FlatList, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 
-const ProductsOverviewScreen = () => {
+import ProductItem from '../../components/shop/ProductItem';
+
+const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
 
     return (
         <FlatList
             data={products}
             keyExtractor={item => item.id}
-            renderItem={itemData => <Text>{itemData.item.title}</Text>}
+            renderItem={itemData => <ProductItem 
+                image={itemData.item.imageUrl} 
+                title={itemData.item.title}
+                price={itemData.item.price}
+                onViewDetail={() => {}}
+                onAddCart={() => {}} />}
         />
     );
 };
