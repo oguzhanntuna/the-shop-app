4172c611484542115343de00a6600d13d3f46d66
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-06-04 17:13:17 +0300

    installing redux thunk and storing products on a server

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,20 +7,39 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-    return {
-        type: CREATE_PRODUCT, 
-        productData: {
-            title,
-            description,
-            imageUrl,
-            price
-        }
-    };
+    return async dispatch => {
+        // any async code you want!
+        const response = await fetch('https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/products.json', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl,
+                price
+            })
+        });
+
+        const resData = await response.json();
+
+        dispatch({
+            type: CREATE_PRODUCT,
+            productData: {
+                id: resData.name,
+                title,
+                description,
+                imageUrl,
+                price
+            }
+        })
+    }
 }
 
 export const updateProduct = (id, title, description, imageUrl) => {
     return {
-        type: UPDATE_PRODUCT, 
+        type: UPDATE_PRODUCT,
         pid: id,
         productData: {
             title,
