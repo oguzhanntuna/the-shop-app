a2990f549e8730fec581b26656192bf171617a09
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-06-04 17:32:33 +0300

    fetching products from the server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,6 +1,34 @@
+import Product from '../../models/product';
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+    return async dispatch => {
+        // any async code you want!
+        const response = await fetch(
+            'https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/products.json'
+        );
+
+        const resData = await response.json();
+        const loadedProducts = [];
+
+        for (const key in resData) {
+            loadedProducts.push(new Product(
+                key,
+                'u1',
+                resData[key].title,
+                resData[key].imageUrl,
+                resData[key].description,
+                resData[key].price
+            ));
+        }
+
+        dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    }
+};
 
 export const deleteProduct = productId => {
     return { type: DELETE_PRODUCT, pid: productId };
@@ -9,7 +37,8 @@ export const deleteProduct = productId => {
 export const createProduct = (title, description, imageUrl, price) => {
     return async dispatch => {
         // any async code you want!
-        const response = await fetch('https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/products.json', {
+        const response = await fetch(
+            'https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/products.json', {
             method: 'POST',
             header: {
                 'Content-Type': 'application/json'
