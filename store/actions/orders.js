dbb5e6aa90b5f5808afafaf4b2bc1c52ca946137
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-06-07 12:38:44 +0300

    storing orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,11 +1,37 @@
-import CartItem from "../../models/cart-item";
-import cart from "../reducers/cart";
-
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-    return { 
-        type: ADD_ORDER, 
-        orderData: { items: cartItems, amount: totalAmount } 
-    };
+    return async dispatch => {
+        const date = new Date();
+        const response = await fetch(
+            'https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/orders/u1.json',
+            {
+                method: 'POST',
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cartItems,
+                    totalAmount,
+                    date: date.toISOString()
+                })
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+
+        const resData = await response.json();
+
+        dispatch({
+            type: ADD_ORDER,
+            orderData: {
+                id: resData.name,
+                items: cartItems,
+                amount: totalAmount,
+                date: date
+            }
+        });
+    }
 };
