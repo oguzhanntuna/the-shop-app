5468b89279bc81e4542f44f6403fd227ecd3523c
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-06-07 10:54:39 +0300

    displaying a loading spinner and handling errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,25 +8,34 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 export const fetchProducts = () => {
     return async dispatch => {
         // any async code you want!
-        const response = await fetch(
-            'https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/products.json'
-        );
+        try {
+            const response = await fetch(
+                'https://the-shop-app-bcf8e-default-rtdb.europe-west1.firebasedatabase.app/products.json'
+            );
 
-        const resData = await response.json();
-        const loadedProducts = [];
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
 
-        for (const key in resData) {
-            loadedProducts.push(new Product(
-                key,
-                'u1',
-                resData[key].title,
-                resData[key].imageUrl,
-                resData[key].description,
-                resData[key].price
-            ));
+            const resData = await response.json();
+            const loadedProducts = [];
+
+            for (const key in resData) {
+                loadedProducts.push(new Product(
+                    key,
+                    'u1',
+                    resData[key].title,
+                    resData[key].imageUrl,
+                    resData[key].description,
+                    resData[key].price
+                ));
+            }
+
+            dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+        } catch (err) {
+            // send to custom analytics server
+            throw err;
         }
-
-        dispatch({ type: SET_PRODUCTS, products: loadedProducts });
     }
 };
 
